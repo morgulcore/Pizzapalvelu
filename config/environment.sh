1834f3753134c6533a20b99bc708e74287363d83
Henrik Lindberg <henrik.lindberg@cs.helsinki.fi>
2017-03-16 15:27:38 +0200

    Thu Mar 16 15:27:38 EET 2017

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="xhexhex"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="pizzapalvelu"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
